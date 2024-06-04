fc97fa10eb77b248c17f6fcafae079802f6bb2fd
analuvicosi <ana.dourado10@etec.sp.gov.br>
2024-06-04 18:00:39 -0300

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,11 +62,55 @@
         <div id="container">
 <!-- Logo -->
 
+
 <section class="logo">
     <a href="{{ route('home')}}">
         <img src="{{ asset('assets/image/logo.png') }}" alt="Nome da sua empresa" class="rotating-logo">
     </a>
 </section>
+<section class="login">
+<a href="{{ route('login')}}">
+    <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="#ffffff"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path
+    d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
+  ></path>
+  <circle cx="12" cy="7" r="4"></circle>
+</svg>
+</a>
+</section>
+
+<section class="carrinho">   
+ <a href="{{ route('carrinho')}}">
+ <svg    xmlns="http://www.w3.org/2000/svg" 
+  width="24"  
+ height="24"  
+ viewBox="0 0 24 24"  
+ fill="none" 
+ stroke="#ffffff"
+ stroke-width="2"
+ stroke-linecap="round" 
+ stroke-linejoin="round" >  
+ <circle cx="9" cy="21" r="1">
+ </circle> 
+ <circle cx="20" cy="21" r="1">
+</circle>   
+<path     d="M1 1h4l2.68 12.32a2 2 0 0 0 2 1.68h9.72a2 2 0 0 0 2-1.68L23 6H6"   >
+</path> 
+</svg>  
+</a>
+ </section>
+
+
     </div>
     </nav>
         <div id="container">
