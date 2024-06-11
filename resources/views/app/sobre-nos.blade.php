c2439965c0427ff2746773f22fb2d5e6aad99abe
Gabriel Henrique <gabriel.barros108@etec.sp.gov.br>
2024-06-11 14:16:52 -0300

    alteração no sobre-nós

--- resources/views/app/sobre-nos.blade.php
+++ resources/views/app/sobre-nos.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
-
+ 
 @section('title')
 <title>Sobre Nós</title>
 @endsection
-
+ 
 @section('body')
-<article class="about">
-    <div class="aboutText">
-        <p>Sobre</p>
-        <p >
-            
-        Fundada em 2023, a Shirt4you surgiu como uma pequena gráfica e  ateliê  de artes.  Ao longo dos anos, crescemos e evoluímos, mas nossa essência permanece a mesma: proporcionar uma experiência única e satisfatória para cada cliente. Com isso nos tornamos uma das maiores lojas on-line de personalização da cidade de são Paulo, com foco em entrega rápida e produtos personalizados de alta qualidade.
-        Hoje, contamos com um amplo portfólio de camisetas personalizadas com os  mais variados artistas internacionais e nacionais. Queremos ser a primeira escolha para quem busca camisas personalizadas, oferecendo não apenas produtos incríveis, mas também uma experiência de compra agradável e memorável. Na Shirt4You, cada cliente é único, e estamos aqui para ajudar você a criar a camisa perfeita que conte sua história.
-        </p>
-    </div>
-</article>
-</main> 
+<main class="container">
+    <article class="about">
+        <div class="aboutText">
+            <h2 class="text-center mb-4 fs-4">Sobre Nós</h2>
+            <p class="lead fs-5">
+                Fundada em 2023, a Shirt4you começou como uma modesta gráfica e ateliê de artes. Com o tempo, evoluímos, mantendo nossa essência de proporcionar experiências únicas e satisfatórias para cada cliente.
+            </p>
+            <p class="fs-5">
+                Hoje, somos uma das principais lojas online de personalização em São Paulo, enfocando entrega rápida e produtos de alta qualidade. Nosso amplo portfólio de camisetas personalizadas inclui obras de variados artistas, tanto internacionais quanto nacionais.
+            </p>
+            <p class="fs-5">
+                Na Shirt4you, cada cliente é único. Estamos comprometidos em oferecer não apenas produtos incríveis, mas também uma experiência de compra agradável e memorável. Estamos aqui para ajudá-lo a criar a camisa perfeita que conte a sua história.
+            </p>
+        </div>
+    </article>
+</main>
 @endsection
+tem menu de contexto
 
 
+tem menu de contexto
